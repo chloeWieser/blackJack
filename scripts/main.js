f9e6bcdfbced4e532700d4ec0784324dbca6fc6c
chloeWieser <chloerosewieser@gmail.com>
2022-03-07 09:36:00 -0500

    Init

---
+++ scripts/main.js
@@ -0,0 +1,268 @@
+const deck = [
+    {
+        rank: 1,
+        suit: "diamonds",
+        imgUrl: "images/ace_of_diamonds.png"
+    },
+    {
+        rank: 2,
+        suit: "diamonds",
+        imgUrl: "images/2_of_diamonds.png"
+    },
+    {
+        rank: 3,
+        suit: "diamonds",
+        imgUrl: "images/3_of_diamonds.png"
+    },
+    {
+        rank: 4,
+        suit: "diamonds",
+        imgUrl: "images/4_of_diamonds.png"
+    },
+    {
+        rank: 5,
+        suit: "diamonds",
+        imgUrl: "images/5_of_diamonds.png"
+    },
+    {
+        rank: 6,
+        suit: "diamonds",
+        imgUrl: "images/6_of_diamonds.png"
+    },
+    {
+        rank: 7,
+        suit: "diamonds",
+        imgUrl: "images/7_of_diamonds.png"
+    },
+    {
+        rank: 8,
+        suit: "diamonds",
+        imgUrl: "images/8_of_diamonds.png"
+    },
+    {
+        rank: 9,
+        suit: "diamonds",
+        imgUrl: "images/9_of_diamonds.png"
+    },
+    {
+        rank: 10,
+        suit: "diamonds",
+        imgUrl: "images/10_of_diamonds.png"
+    },
+    {
+        rank: 11,
+        suit: "diamonds",
+        imgUrl: "images/jack_of_diamonds.png"
+    },
+    {
+        rank: 12,
+        suit: "diamonds",
+        imgUrl: "images/queen_of_diamonds.png"
+    },
+    {
+        rank: 13,
+        suit: "diamonds",
+        imgUrl: "images/king_of_diamonds.png"
+    },
+
+    {
+        rank: 1,
+        suit: "clubs",
+        imgUrl: "images/ace_of_clubs.png"
+    },
+    {
+        rank: 2,
+        suit: "clubs",
+        imgUrl: "images/2_of_clubs.png"
+    },
+    {
+        rank: 3,
+        suit: "clubs",
+        imgUrl: "images/3_of_clubs.png"
+    },
+    {
+        rank: 4,
+        suit: "clubs",
+        imgUrl: "images/4_of_clubs.png"
+    },
+    {
+        rank: 5,
+        suit: "clubs",
+        imgUrl: "images/5_of_clubs.png"
+    },
+    {
+        rank: 6,
+        suit: "clubs",
+        imgUrl: "images/6_of_clubs.png"
+    },
+    {
+        rank: 7,
+        suit: "clubs",
+        imgUrl: "images/7_of_clubs.png"
+    },
+    {
+        rank: 8,
+        suit: "clubs",
+        imgUrl: "images/8_of_clubs.png"
+    },
+    {
+        rank: 9,
+        suit: "clubs",
+        imgUrl: "images/9_of_clubs.png"
+    },
+    {
+        rank: 10,
+        suit: "clubs",
+        imgUrl: "images/10_of_clubs.png"
+    },
+    {
+        rank: 11,
+        suit: "clubs",
+        imgUrl: "images/jack_of_clubs.png"
+    },
+    {
+        rank: 12,
+        suit: "clubs",
+        imgUrl: "images/queen_of_clubs.png"
+    },
+    {
+        rank: 13,
+        suit: "clubs",
+        imgUrl: "images/king_of_clubs.png"
+    },
+
+
+    {
+        rank: 1,
+        suit: "hearts",
+        imgUrl: "images/ace_of_hearts.png"
+    },
+    {
+        rank: 2,
+        suit: "hearts",
+        imgUrl: "images/2_of_hearts.png"
+    },
+    {
+        rank: 3,
+        suit: "hearts",
+        imgUrl: "images/3_of_hearts.png"
+    },
+    {
+        rank: 4,
+        suit: "hearts",
+        imgUrl: "images/4_of_hearts.png"
+    },
+    {
+        rank: 5,
+        suit: "hearts",
+        imgUrl: "images/5_of_hearts.png"
+    },
+    {
+        rank: 6,
+        suit: "hearts",
+        imgUrl: "images/6_of_hearts.png"
+    },
+    {
+        rank: 7,
+        suit: "hearts",
+        imgUrl: "images/7_of_hearts.png"
+    },
+    {
+        rank: 8,
+        suit: "hearts",
+        imgUrl: "images/8_of_hearts.png"
+    },
+    {
+        rank: 9,
+        suit: "hearts",
+        imgUrl: "images/9_of_hearts.png"
+    },
+    {
+        rank: 10,
+        suit: "hearts",
+        imgUrl: "images/10_of_hearts.png"
+    },
+    {
+        rank: 11,
+        suit: "hearts",
+        imgUrl: "images/jack_of_hearts.png"
+    },
+    {
+        rank: 12,
+        suit: "hearts",
+        imgUrl: "images/queen_of_hearts.png"
+    },
+    {
+        rank: 13,
+        suit: "hearts",
+        imgUrl: "images/king_of_hearts.png"
+    },
+
+    {
+        rank: 1,
+        suit: "spades",
+        imgUrl: "images/ace_of_spades.png"
+    },
+    {
+        rank: 2,
+        suit: "spades",
+        imgUrl: "images/2_of_spades.png"
+    },
+    {
+        rank: 3,
+        suit: "spades",
+        imgUrl: "images/3_of_spades.png"
+    },
+    {
+        rank: 4,
+        suit: "spades",
+        imgUrl: "images/4_of_spades.png"
+    },
+    {
+        rank: 5,
+        suit: "spades",
+        imgUrl: "images/5_of_spades.png"
+    },
+    {
+        rank: 6,
+        suit: "spades",
+        imgUrl: "images/6_of_spades.png"
+    },
+    {
+        rank: 7,
+        suit: "spades",
+        imgUrl: "images/7_of_spades.png"
+    },
+    {
+        rank: 8,
+        suit: "spades",
+        imgUrl: "images/8_of_spades.png"
+    },
+    {
+        rank: 9,
+        suit: "spades",
+        imgUrl: "images/9_of_spades.png"
+    },
+    {
+        rank: 10,
+        suit: "spades",
+        imgUrl: "images/10_of_spades.png"
+    },
+    {
+        rank: 11,
+        suit: "spades",
+        imgUrl: "images/jack_of_spades.png"
+    },
+    {
+        rank: 12,
+        suit: "spades",
+        imgUrl: "images/queen_of_spades.png"
+    },
+    {
+        rank: 13,
+        suit: "spades",
+        imgUrl: "images/king_of_spades.png"
+    },
+]
+
+console.log(deck)
